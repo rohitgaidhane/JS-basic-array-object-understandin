e40f04fd4239a61c6ba4c3e631124b257e9467e2
rohitgaidhane <rohitgaidhane123@gmail.com>
2023-12-30 21:39:24 +0530

    changes for array basic

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
-// Import stylesheets
-import './style.css';
+let arr = [
+  { name: "user1", age: 28 }, // arr[0]
+  { name: "user2", age: 78 }, // arr[1]
+  { name: "user3", age: 38 }, // arr[2]
+  { name: "user4", age: 25 }, // arr[3]
+];
 
-// Write Javascript code!
-const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+function filterArray() {
+  let tempArr = [];  // temp array 
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].age > 30) {
+      tempArr.push(arr[i]);
+    }
+  }
+  return tempArr;
+}
+
+// console.log(arr);
+let ageGreaterThanThirty  = filterArray();
+console.log(ageGreaterThanThirty,"user with age greater than 30");
